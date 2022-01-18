f9088715d59604ae648891d5a7ed3f3b7dfd6e80
mauriziobrt <45272301+mauriziobrt@users.noreply.github.com>
2022-01-18 16:48:10 +0100

    Working Code

--- source/projects/me.ddsp_tilde/ddsp_model.cpp
+++ source/projects/me.ddsp_tilde/ddsp_model.cpp
@@ -42,26 +42,42 @@ void DDSPModel::perform(float *pitch, float *loudness, float *out_buffer, int bu
     torch::NoGradGuard no_grad;
     if (m_loaded)
     {
+//        std::cout << "The pitch value is: " << *pitch << std::endl;
+//        std::cout << "The loudness value is: " << *loudness << std::endl;
+        //auto options =torch::TensorOptions().dtype(torch::kFloat64);
         //from_blob = use exterrnal created data, (data, {})
         auto pitch_tensor = torch::from_blob(pitch, {1, buffer_size, 1});
         //std::cout << "TORCH tiene questo pitch tensor" << pitch_tensor << std::endl;
         auto loudness_tensor = torch::from_blob(loudness, {1, buffer_size, 1});
         //std::cout << "TORCH tiene questo loudness tensor" << loudness_tensor << std::endl;
-
+        //std::cout<< "Size in bytes of loudness tensor "<< loudness_tensor.element_size() << std::endl;
         pitch_tensor = pitch_tensor.to(DEVICE);
         loudness_tensor = loudness_tensor.to(DEVICE);
         std::vector<torch::jit::IValue> inputs = {pitch_tensor, loudness_tensor};
+        
+        //std::cout <<"Ivalue 2 " << inputs << "\n";
         //std::cout << "Tensor in input -> " << inputs << std::endl;
-
-        //QUA SI ROMPE TUTTO!!!!!!!!!!!!!!!!!! Perchè probabilmente in pure data riceve valori mappati diversamente
-        //auto out_tensor = m_scripted_model.forward(inputs).toTensor();
-        at::Tensor out_tensor = m_scripted_model.forward(inputs).toTensor();
-        //std::cout << "Tensor in output ->  " << out_tensor << std::endl;
+        
+        //ok allora ricevo roba a dimensione 8 bytes, però il modello non la capisce e dice che non va bene
+        //std::cout << "The module " << *m_scripted_model.type() << "\n";
+        
+        auto out_tensor = m_scripted_model.forward(inputs).toTensor();
         out_tensor = out_tensor.to(DEVICE);
+        //std::cout<< "Size in bytes of out tensor "<< out_tensor.element_size() << std::endl;
 
+
+        //returns address of first element of tensor
         auto out = out_tensor.contiguous().data_ptr<float>();
-        //std::cout << "Torch array di output: "<< *out << std::endl;
+
+        //auto gino = out_tensor.item<double>();
+        //std::cout << gino << std::endl;
+        //auto* out1 = static_cast<double*>(out);
+
+        //forse devo sapere la size di out per capire dove inizia e finisce
+        //smette di funzionare appena riceve i valor di pitch e loudness.
         memcpy(out_buffer, out, buffer_size * sizeof(float));
+        //std::copy(out, buffer_size + out , out_buffer);
+        //std::cout << "Adesso l'output buffer è uguale a: " << *out_buffer <<std::endl;
     }
     else {
         std::cout << "Model not loaded. " << std::endl;
--- source/projects/me.ddsp_tilde/me.ddsp_tilde.cpp
+++ source/projects/me.ddsp_tilde/me.ddsp_tilde.cpp
@@ -12,7 +12,7 @@
 using namespace c74::min;
 
 
-class me_ddsp_tilde : public object<me_ddsp_tilde>, public vector_operator<> {
+class me_ddsp_tilde : public object<me_ddsp_tilde>, public sample_operator<2,1> {
 public:
     MIN_DESCRIPTION	{"DDSP Wrapper for MAX/MSP"};
     MIN_TAGS		{"utilities"};
@@ -29,14 +29,14 @@ public:
         cout << "Value of m_head at object creation:" << m_head << endl;
     };
     
-    message<> dspsetup { this, "dspsetup",
-        MIN_FUNCTION {
-            number samplerate = args[0];
-            int vectorsize = args[1];
-            m_one_over_samplerate = 1.0 / samplerate;
-            return {};
-        }
-    };
+//    message<> dspsetup { this, "dspsetup",
+//        MIN_FUNCTION {
+//            number samplerate = args[0];
+//            //int vectorsize = args[1];
+//            m_one_over_samplerate = 1.0 / samplerate;
+//            return {};
+//        }
+//    };
     
     //this is supposed to take a load message which opens a finder instance where it is possible to choose the model
     //the argument of the load function must be a path to the model.
@@ -77,57 +77,64 @@ public:
             
 
     // Questo viene eseguito in continuazione a dsp acceso
-    void operator()(audio_bundle input, audio_bundle output){
-
-            auto* in1 = input.samples(0);
-            //cout << "Value inside in1: "<< *in1 << endl;
-            auto* in2 = input.samples(1);
-            //cout << "Value inside in2: "<< *in2 << endl;
-
-            //invece questo in pd riporta dei valori tipo 81920
-            auto* out = output.samples(0);
+    sample operator()(sample in1, sample in2){
             
-            //cout << "Value out: "<< *out << endl;
+            m_pitch_buffer[m_head] = in1;
+            m_loudness_buffer[m_head] = in2;
             
-            //ho sbagliato a leggere i dati e comunque sono di tipo t_sample
-            //    The w1 value is : 24679424 and -272681888
-            //    The w2 value is : 4884576 and 3186080
-            //    The w3 value is : 4885632 and 3186080
-            //    The w4 value is : 4885632 and 3186080
-            //    The w5 value is : 64 and 3186080
-
-           
+//            auto* in1 = input.samples(0);
+//            //cout << "Value inside in1: "<< *in1 << endl;
+//            auto* in2 = input.samples(1);
+//            //cout << "Value inside in2: "<< *in2 << endl;
+//            auto* out = output.samples(0);
+                       
             //semplice +~
 //            for (auto i = 0; i < input.frame_count(); i++){
 //            out[i] = in1[i] + in2[i];
 //            }
 
             int n = vector_size();
+            
+            //cout << "Il valore di n è: " << n << "\n"<< endl;
+            
 //            the length of this signal-vector ().s_n.
 
             // primo step: copio nei buffer il valore attuale di pitch e loudness
-
-            memcpy(m_pitch_buffer + m_head, in1, n * sizeof(float));
-            memcpy(m_loudness_buffer + m_head, in2, n * sizeof(float));
-            // ci vuole del preprocessing per dividere da una parte il pitch e dall'altra la loudness
-            //e copio il valore di m_out_buffer in out
-            memcpy(out, m_out_buffer + m_head, n * sizeof(float));
+            
+            //std::copy(in1, in1 + n, m_pitch_buffer);
+            
+            //il problema è che qua usiamo i double mentre in pd usiamo i float
+            
+            //std::copy(in2, in2 + n, m_loudness_buffer);
+            
+            //std::copy(m_out_buffer, m_out_buffer + n, out);
+            
+            
+//            //ciò che entra in nel buffer è sbagliato, dentro in1 il valore è 46.1 dentro pitch_buffer 2.00
+//            memcpy(m_pitch_buffer + m_head, in1, n * sizeof(float));
+//            for(auto i = 0; i < input.frame_count(); i++)
+//
+//
+//            //printf("Il buffer del pitch tiene %f \n", *m_pitch_buffer);
+//            // m_pitch_buffer += m_head;
+//            memcpy(m_loudness_buffer + m_head, in2, n * sizeof(double));
+//            // ci vuole del preprocessing per dividere da una parte il pitch e dall'altra la loudness
+//            //e copio il valore di m_out_buffer in out
+//            memcpy(out, m_out_buffer + m_head, n * sizeof(double));
             //cout << "Ma l'out che contiene all'inizio? .. " << *m_out_buffer << "  Ah ok..."<< endl;
 
             
-            m_head = m_head + n;
+            m_head += n;
                         
-            //cout<< m_head << endl;
-
+            // i valori che arrivano al modello sono sballati, all'ingresso qua sono giusti, poi si sballano quando vengono copiati nel buffer.
             if (!(m_head % B_SIZE))
             {
             // io credo che esegua l'operazione ogni tot vettori e non sempre
-                //cout << "Sto all'inizio perchè m_head%B_SIZe è = a 0." << m_head % B_SIZE << endl;
                 if (compute_thread)
                     {
                         //stops the thread execution:
                         compute_thread->join();
-                        //ad ogni step lui joina il thread per qualche strana ragione, cioè creo thread e lo rimetto dentro, creo thread e lo rimetto dentro.
+                        //ad ogni step lui joina il thread per qualche ragione, cioè creo thread e lo rimetto dentro main, creo thread e lo rimetto dentro.
                     }
                 int model_head = ((m_head + B_SIZE) % (2 * B_SIZE));
                 //cout << "Value of " <<model_head <<endl;
@@ -136,19 +143,15 @@ public:
                                              m_loudness_buffer + model_head,
                                              m_out_buffer + model_head,
                                              B_SIZE);
-            ///Quindi un problema sta qui, quando vengono ricevuti i buffer di input ma in output si blocca il processo e ritorna un valore nan
-            // questo perchè i valori di input sono sballati, non si aspetta i valori che arrivano da max
-            cout << "Contenuto out buffer:  " << *m_out_buffer << " Se nan => valori in ingresso sbagliati. "<< endl;
+            //Il problema è il casting dei valori
+            //cout << "Contenuto out buffer:  " << *m_out_buffer << " Se nan => valori in ingresso sbagliati. "<< endl;
             //compute thread prende i 2 buffer di ingresso applica il processo e poi copia dentro m_out_buffer.
                 m_head = m_head % (2* B_SIZE);
-            //cout << "Now m_head is: " << m_head << endl;
             }
-//            else {
-//            cout << "Sto alla fine perchè m_head%B_SIZe è diverso da 0. Infatti è :" << m_head % B_SIZE << endl;
-//            }
+            return m_out_buffer[m_head];
     }
     private:
-        double m_one_over_samplerate {1.0};
+        //double m_one_over_samplerate {1.0};
         float m_pitch_buffer[2 * B_SIZE];
         float m_loudness_buffer[2 * B_SIZE];
         float m_out_buffer[2 * B_SIZE];
@@ -158,7 +161,6 @@ public:
         std::thread *compute_thread;
             
         DDSPModel *model;
-            
 };
 
 
